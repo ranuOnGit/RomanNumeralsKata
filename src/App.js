a17e541d56d03d342a0c4bb2f0545fce6e94d83a
Ranu <ranu.agrwl@gmail.com>
2021-04-19 14:13:54 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [ roman, setRoman ] = useState( '' )
+  const [ integer, setInteger ] = useState( 0 )
+  const [modal, setModal] = useState(false)
+  
+  const handleChange = ( e ) => {
+    setRoman((e.target.value).toUpperCase())
+  }
+  
+  const handleSubmit = ( e ) => {
+    e.preventDefault()
+
+    const comparisonArray = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
+
+    const romanArray = roman.split( '' )
+    
+    let totalNumber = 0
+
+    let firstLetter;
+    let firstNumber;
+    let nextLetter;
+    let nextNumber;
+
+    for ( let i = 0; i < romanArray.length; i++ ){
+      firstLetter = romanArray[ i ]
+      firstNumber = comparisonArray[ firstLetter ]      
+      nextLetter = romanArray[ i + 1 ]
+      nextNumber = comparisonArray[ nextLetter ]
+      
+      if ( firstNumber < nextNumber ) {
+        totalNumber -= firstNumber
+      } else {
+        totalNumber += firstNumber
+      }
+    }
+
+    if ( !totalNumber ) {
+      setModal( true )
+    }
+    setInteger( totalNumber )
+    
+  }
+
+   useEffect(() => {
+     const timer = setTimeout(() => {
+       setModal(false);
+     }, 1000);
+     return () => clearTimeout(timer);
+   } );
+  
+  useEffect(() => {
+    const timer = setTimeout( () => {
+      setRoman('');
+      setInteger(0);
+    }, 3000);
+    return () => clearTimeout(timer);
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2>Convert Roman Numeral to Integer</h2>
+      <div className='roman'>
+        <input type='text' placeholder='roman numeral' value={roman} onChange={handleChange} />
+        <button onClick={handleSubmit}>Submit</button>
+        <div>
+          {modal && (
+            <h4 className='roman-h4'>Please enter a valid Roman Numeral!</h4>
+          )}
+        </div>
+      </div>
+      <div className='integer'>
+        <h2 className='int-h3'>INTEGER : {integer}</h2>
+      </div>
+      <h5>a react app</h5>
     </div>
   );
 }
 
-export default App;
+export default App
